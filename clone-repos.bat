320a3b2be545cba7499c46dc9dba6b4d50cfe89f
Alberto Sierra <alberto.sierra@murciaeduca.es>
2022-06-02 10:02:55 +0200

    sql-validator integration
    restarting java-valida on save
    
    connecting feedback-manager to MongoDB
    
    updating branches in clone script files

--- clone-repos.bat
+++ clone-repos.bat
@@ -1,26 +1,27 @@
 @echo off
-FOR /F "eol=# tokens=*" %%i IN .env DO SET %%i
+FOR /F "eol=# tokens=*" %%i IN (.env) DO SET %%i
 
 cd ./tsugi
 mkdir src_volume
 cd src_volume
 git clone --branch codetest-customizations          https://github.com/KA226-COVID/tsugi.git
-git clone --branch feat/external-validators         https://github.com/KA226-COVID/codetest.git
+git clone --branch master         https://github.com/KA226-COVID/codetest.git
 cd ..
 cd ..
 
 cd ./central-repository
 mkdir src_volume
 cd src_volume
-git clone --branch feat/files-mgmt                  https://github.com/KA226-COVID/questions-storage.git
+git clone --branch master                  https://github.com/KA226-COVID/questions-storage.git
 cd ..
 cd ..
 
 cd ./validators
 mkdir src_volume
 cd src_volume
-git clone --branch main                             https://%GITHUB_TOKEN%@github.com/KA226-COVID/feedback-manager.git
-git clone --branch deployment                       https://%GITHUB_TOKEN%@github.com/KA226-COVID/xml-evaluator.git
+git clone --branch master                             https://%GITHUB_TOKEN%@github.com/KA226-COVID/feedback-manager.git
+git clone --branch master                       https://%GITHUB_TOKEN%@github.com/KA226-COVID/xml-evaluator.git
 git clone --branch master                           https://%GITHUB_TOKEN%@github.com/KA226-COVID/java-evaluator.git
+git clone --branch master                           https://%GITHUB_TOKEN%@github.com/KA226-COVID/sql-evaluator.git
 cd ..
 cd ..
--- tsugi/config_codetest_docker.php
+++ tsugi/config_codetest_docker.php
@@ -1,15 +1,9 @@
 <?php
-
-// Make this require relative to the parent of the current folder
-// http://stackoverflow.com/exercises/24753758
-
-
 require_once dirname(__DIR__) . "/config.php";
 require 'vendor/autoload.php';
 
 $CFG->codetestRootDir = dirname(__FILE__);
 $CFG->codetestBasePath = __DIR__;
-
 
 $CFG->twig = array(
     'viewsPath' => __DIR__ . "/views",
@@ -26,10 +20,29 @@ $CFG->repositoryUrl = getenv('SPRING_CODETEST_URL').":".getenv('SPRING_CODETEST_
 
 $CFG->type = [
     "PHP" => "PHP",
-    "MYSQL" => "MYSQL",
+    "SQL" => "SQL",
     "Python" => "Python",
     "Java" => "Java"
 ];
+
+$CFG->validators = array(
+    "Java" => [
+        "name" => 'Java',
+        "baseUrl" => "http://java-validator:3000/",
+    ],
+    "PHP" => [
+        "name" => 'PHP',
+        "baseUrl" => "http://{$CFG->repositoryUrl}/",
+    ],
+    "SQL" => [
+        "name" => "SQL",
+        "baseUrl" => "http://sql-validator:3000/",
+    ],
+    "Python" => [
+        "name" => 'Python',
+        "baseUrl" => "http://{$CFG->repositoryUrl}/",
+    ],
+);
 
 $CFG->apiConfigs = [
     "spring-repo" => [
@@ -44,82 +57,20 @@ $CFG->apiConfigs = [
         "user" => getenv('AK_USER'),
         "pass" => getenv('AK_PASS'),
     ],
-    "xml-validator" => [
-        // MUST HAVE A TRAILING SLASH
-        "baseUrl" => "http://".getenv('XML_EVALUATOR_URL').":".getenv('XML_EVALUATOR_PORT')."/",
-    ],
 ];
 
-$CFG->programmingLanguajes = array(
-    'PHP',
-    'Java',
-    'Javascript',
-    'Python'
-);
-
-$CFG->CT_Types = array(
-    'formsPath' => 'exercise/forms/',
-    'studentsPath' => 'exercise/students/',
-    'types' => array(
-        'MYSQL' => array(
-            'name' => 'sql',
-            'class' => \CT\CT_ExerciseSQL::class,
-            'instructorForm' => 'exerciseSQLForm.php.twig',
-            'studentView' => 'exerciseSQLStudent.php.twig',
-            'sqlTypes' => array('SELECT', 'DML', 'DDL'),
-            'dbConnections' => array(
-                array(
-                    'name' => 'MySQL',
-                    'dbDriver' => 'mysql',
-                    'dbHostName' => 'tsugi-mysql-db',
-                    'dbPort' => 3306,
-                    'dbUser' => 'tsugi_root',
-                    'dbPassword' => 'tpass',
-                    'onFly' => array(
-                        'allowed' => true,
-                        'userPrefix' => 'JUEZ',
-                        'createIsolateUserProcedure' => 'tsugi.CREATEISOLATEUSER',
-                        'dropIsolateUserProcedure' => 'tsugi.DROPISOLATEUSER',
-                    ),
-                ),
-                array(
-                    'name' => 'Oracle',
-                    'dbDriver' => 'oci',
-                    'dbHostName' => 'localhost',
-                    'dbPort' => 1521,
-                    'dbSID' => 'dbSID',
-                    'dbUser' => 'oraUser',
-                    'dbPassword' => 'oraPass',
-                    'onFly' => array(
-                        'allowed' => true,
-                        'userPrefix' => 'userPrefix',
-                        'createIsolateUserProcedure' => 'CREATEISOLATEUSER', // Show definition at the end
-                        'dropIsolateUserProcedure' => 'DROPISOLATEUSER', // Show definition at the end
-                    ),
-                ),
-                array(
-                    'name' => 'SQLite',
-                    'dbDriver' => 'sqlite',
-                    'dbFile' => '/path/to/file.sq3 or :memory:',
-                    'dbUser' => '',
-                    'dbPassword' => '',
-                    'onFly' => array(
-                        'allowed' => true,
-                    ),
-                ),
-            ),
-        ),
-        'programming' => array(
-            'name' => 'programming',
-            'class' => \CT\CT_ExerciseCode::class,
-            'instructorForm' => 'exerciseCodeForm.php.twig',
-            'studentView' => 'exerciseCodeStudent.php.twig',
-            'timeout' => 5,
-            'codeLanguages' => array(
-                array('name' => 'Java', 'ext' => 'xml', 'command' => null, 'stdin' => false),
-            ),
-        ),
+$CFG->ExerciseProperty = array(
+    'name' => 'programming',
+    'class' => \CT\CT_ExerciseCode::class,
+    'instructorForm' => 'exercise/forms/exerciseCodeForm.php.twig',
+    'studentView' => 'exercise/students/exerciseCodeStudent.php.twig',
+    'timeout' => 5,
+    'codeLanguages' => array(
+        array('name' => 'Java'),
+        // array('name' => 'PHP', 'ext' => 'xml', 'command' => null, 'stdin' => false)
+        
     ),
+    
 );
 
 $CFG->difficulty = array(
